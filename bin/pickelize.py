Fixed one more overflow problem

bin/pickelize.py
@@ -81,11 +81,20 @@ for i, k in tqdm(list(enumerate(pdb_names))):
 output = np.zeros((len(pdb_names), len(useable_keys)*2), dtype=np.float16) # maybe use dtype=np.int8) to reduce memory
 print("Output array size (pdb x one-hot output): ", output.shape)
 print("Filling output array")
+np.seterr(all="raise")
 for i, k in tqdm(list(enumerate(pdb_names))):
     for j, key in enumerate(useable_keys):
         if key in data[k]['values'].keys():
             output[i, j*2] = 1
-            output[i, j*2+1] = data[k]['values'][key]
+            try:
+                output[i, j*2+1] = data[k]['values'][key]
+            except FloatingPointError as someEx:
+                print("YOLO")
+                print(k)
+                print("Error in ", k, " ", key)
+                print(someEx)
+                print(data[k]['values'][key]) 
+                output[i, j*2+1] = np.finfo(np.half).max
         else:
             output[i, j*2] = 0
             output[i, j*2+1] = 0

bin/tidy.py
@@ -20,9 +20,11 @@ class PdbParseException (Exception):
 
 # Open the pdb.toml file
 with gzip.open("pdb.toml.gz", "rt") as f:
-    print("Loading data...")
+    print("Loading data...", end="", flush=True)
     pdb = toml.load(f)
+    print("done")
 
+print("processing")
 err = open("tidy.err", "wt")
 
 # Sequence lenght for some stats
@@ -39,6 +41,7 @@ with open("training.toml", "wt") as f:
     # The set of all hash keys
     key_count = {}
     # Process each top level key
+    print("Writing training.toml")
     for key in tqdm.tqdm(pdb.keys()):
         try: 
             # Get the value
@@ -150,11 +153,11 @@ with open("training.toml", "wt") as f:
 
             # if any value is greater or smaller than what numpy.half can hold, continue
             for val in values.values():
-                if val > np.finfo(np.half).max:
-                    #raise PdbParseException(f"ERROR 59: [{key}] Is not interesting, value too large: {crco}".format(key=key, crco=crco))
+                if val >= np.finfo(np.half).max:
+                    raise PdbParseException(f"ERROR 59: [{key}] Is not interesting, value too large: {crco}".format(key=key, crco=crco))
                     continue
-                if val < np.finfo(np.half).min:
-                    #raise PdbParseException(f"ERROR 60: [{key}] Is not interesting, value too small: {crco}".format(key=key, crco=crco))
+                if val <= np.finfo(np.half).min:
+                    raise PdbParseException(f"ERROR 60: [{key}] Is not interesting, value too small: {crco}".format(key=key, crco=crco))
                     continue
 
             solvent = pdb[key]["crystal_conditions"]