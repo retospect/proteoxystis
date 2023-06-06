85f79747ceb81fcda744ead3d822a720ba7ad045
Arpitha Rao <69826982+arpi5191@users.noreply.github.com>
2023-06-06 00:11:01 -0700

    Add files via upload
    Added Plotting, Fixed training and validation accuracies, Removed early stopping from validation

--- bin/ANN.py
+++ bin/ANN.py
@@ -1,11 +1,12 @@
+import os
 import torch
 from train import *
 import pandas as pd
 import torch.nn as nn
 import torch.optim as optim
+import matplotlib.pyplot as plt
 import torch.utils.data as data
 import torch.nn.functional as F
-from sklearn.model_selection import KFold
 from torch.utils.data import TensorDataset
 from sklearn.model_selection import cross_val_score
 from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score
@@ -14,15 +15,15 @@ def data_split(seqs_train, output_train, seqs_test, output_test):
     train_dataset = TensorDataset(torch.tensor(seqs_train), torch.tensor(output_train))
 
     size = len(train_dataset)
-    train_size = int(0.8 * size)
+    train_size = int(0.9 * size)
     val_size = size - train_size
 
     train_dataset, val_dataset = torch.utils.data.random_split(train_dataset, [train_size, val_size])
     test_dataset = TensorDataset(torch.tensor(seqs_test), torch.tensor(output_test))
 
-    train_loader = torch.utils.data.DataLoader(train_dataset, batch_size=8, shuffle=True)
-    val_loader = torch.utils.data.DataLoader(val_dataset, batch_size=8, shuffle=False)
-    test_loader = torch.utils.data.DataLoader(test_dataset, batch_size=8, shuffle=False)
+    train_loader = torch.utils.data.DataLoader(train_dataset, batch_size=128, shuffle=True)
+    val_loader = torch.utils.data.DataLoader(val_dataset, batch_size=128, shuffle=False)
+    test_loader = torch.utils.data.DataLoader(test_dataset, batch_size=128, shuffle=False)
 
     return train_loader, val_loader, test_loader
 
@@ -61,6 +62,11 @@ class ANN(nn.Module):
 def train_model(model, optimizer, epochs, train_loader, val_loader):
     model.train()
 
+    prev_train_accuracy = float(0)
+
+    train_losses = []
+    val_losses = []
+
     train_predicts = []
     train_targets = []
 
@@ -70,7 +76,6 @@ def train_model(model, optimizer, epochs, train_loader, val_loader):
     for epoch in range(epochs):
 
         batch_idx = 0
-        prev_accuracy = float(0)
 
         for batch in train_loader:
 
@@ -94,8 +99,9 @@ def train_model(model, optimizer, epochs, train_loader, val_loader):
 
             accuracy = accuracy_score(train_targets, train_predicts) * 100
 
-            if accuracy >= prev_accuracy:
-                prev_accuracy = accuracy
+            if accuracy >= prev_train_accuracy:
+                train_losses.append(loss)
+                prev_train_accuracy = accuracy
             else:
                 break
 
@@ -108,8 +114,6 @@ def train_model(model, optimizer, epochs, train_loader, val_loader):
 
             batch_idx = 0
 
-            prev_accuracy = 0
-
             for batch in val_loader:
 
                 target = batch[0].float()
@@ -121,6 +125,8 @@ def train_model(model, optimizer, epochs, train_loader, val_loader):
 
                 batch_idx += 1
 
+                val_losses.append(loss)
+
                 predictions = torch.argmax(output, dim=1)
                 targets = torch.argmax(target, dim=1)
 
@@ -128,11 +134,6 @@ def train_model(model, optimizer, epochs, train_loader, val_loader):
                 val_targets.extend(targets)
 
                 accuracy = accuracy_score(val_targets, val_predicts) * 100
-
-                if accuracy >= prev_accuracy:
-                    prev_accuracy = accuracy
-                else:
-                    break
 
                 print("At batch number {b} in epoch {e} the validation loss is {l:.4f} and the validation accuracy is {a:.4f}%".
                                                                             format(b=batch_idx, e=epoch, l=loss, a=accuracy))
@@ -144,7 +145,7 @@ def train_model(model, optimizer, epochs, train_loader, val_loader):
 
     print("Training is complete")
 
-    return final_training_accuracy, final_validation_accuracy
+    return train_losses, val_losses, final_training_accuracy, final_validation_accuracy
 
 def test_model(model, test_loader):
 
@@ -154,8 +155,11 @@ def test_model(model, test_loader):
     batch_idx = 0
 
     for batch in test_loader:
+
         model.eval()
+
         with torch.no_grad():
+
             target = batch[0].float()
             data = batch[1].float()
 
@@ -181,20 +185,42 @@ def test_model(model, test_loader):
 
     return final_testing_accuracy
 
+def plot_loss(train_losses, val_losses, folder, filename):
+    figure, axis = plt.subplots(1, 2, figsize=(10, 5))
+
+    axis[0].plot(range(len(train_losses)), train_losses)
+    axis[0].set_title("Training Loss over Epochs")
+
+    axis[1].plot(range(len(val_losses)), val_losses)
+    axis[1].set_title("Validation Loss over Epochs")
+
+    figure.subplots_adjust(wspace=0.5)
+
+    image_path = os.path.join(folder, filename)
+    plt.savefig(image_path)
+
 def main():
     metadata, seqs_train, output_train, seqs_test, output_test, relevant_test, relevant_train = load_data()
 
     train_loader, val_loader, test_loader = data_split(seqs_train, output_train, seqs_test, output_test)
 
-    epochs = 20
+    epochs = 5
     learning_rate = 0.001
     weight_decay = 0.01
 
     model = ANN()
     optimizer = torch.optim.SGD(model.parameters(), lr=learning_rate, weight_decay=weight_decay)
 
-    final_training_accuracy, final_validation_accuracy = train_model(model, optimizer, epochs, train_loader, val_loader)
+    train_losses, val_losses, final_training_accuracy, final_validation_accuracy = train_model(model, optimizer, epochs, train_loader, val_loader)
     final_testing_accuracy = test_model(model, test_loader)
+
+    train_losses = torch.tensor(train_losses).detach().numpy()
+    val_losses = torch.tensor(val_losses).detach().numpy()
+
+    folder = '/Users/arpitha/Documents/cse144/Final_Project/proteoxystis/bin'
+    filename = 'Losses'
+
+    plot_loss(train_losses, val_losses, folder, filename)
 
     print("The final training accuracy is {ftra:.4f}%".format(ftra=final_training_accuracy))
     print("The final validation accuracy is {fva:.4f}%".format(fva=final_validation_accuracy))
@@ -202,3 +228,8 @@ def main():
 
 if __name__ == "__main__":
     main()
+
+# Citations:
+# https://towardsdatascience.com/building-neural-network-using-pytorch-84f6e75f9a
+# https://www.tutorialspoint.com/how-to-plot-a-graph-in-python
+# https://www.geeksforgeeks.org/plot-multiple-plots-in-matplotlib/
